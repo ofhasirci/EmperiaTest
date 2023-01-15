bc146177ccd748ef7723bc3a3a66a4e3a2d30d1f
Faruk Hasirci <ofhasirci@gmail.com>
2023-01-15 23:52:30 +0300

    Initial Unreal Engine project setup

---
+++ Source/EmperiaTest/EmperiaTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmperiaTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMPERIATEST_API AEmperiaTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
